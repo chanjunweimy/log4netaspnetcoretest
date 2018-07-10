984349145f0c4a3f45385b523765b901f2a7352f
Chan Jun Wei <chanjunweimy@gmail.com>
2018-07-10 16:20:58 +0800

    Template to test Log4Net Extension

---
+++ NetCoreWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace NetCoreWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        private ILogger Logger { get; }
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            Logger = logger;
+            Logger.LogDebug("Home controller is constructed");
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Index()
+        {
+            Logger.LogDebug("Hi");
+            return new string[] { "Hello", "World" };
+        }
+    }
+}
---
+++ NetCoreWebApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace NetCoreWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Directory.SetCurrentDirectory(exeDir);
+
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
